6cddf0801a588d2c85e43d4141ca06976fad745f
sunchunqiang <circleci@ea908549885d>
2019-03-14 00:01:50 +0000

    [bump version 2.12.5]

--- aws-android-sdk-core/src/test/java/com/amazonaws/util/VersionInfoUtilsTest.java
+++ aws-android-sdk-core/src/test/java/com/amazonaws/util/VersionInfoUtilsTest.java
@@ -24,7 +24,7 @@ public class VersionInfoUtilsTest {
 
     @Test
     public void getVersion() {
-        assertEquals("2.12.3", VersionInfoUtils.getVersion());
+        assertEquals("2.12.5", VersionInfoUtils.getVersion());
     }
 
     @Test
